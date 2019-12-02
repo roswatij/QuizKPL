2bc515c0645a48a7657165652834b8eda9fe50c8
SB604-15 <SB604-15@DESKTOP-F0E9H4G>
2019-12-02 13:30:02 +0700

    aaaa

---
+++ QuizKPL/src/main/java/id/ac/unpas/QuizKPL/App.java
@@ -0,0 +1,13 @@
+package id.ac.unpas.QuizKPL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
