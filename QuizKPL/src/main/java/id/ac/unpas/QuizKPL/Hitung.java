20d4c2f42c755701403e65c689701cd5f324d6b1
SB604-15 <SB604-15@DESKTOP-F0E9H4G>
2019-12-02 13:42:49 +0700

    fungsi

---
+++ QuizKPL/src/main/java/id/ac/unpas/QuizKPL/Hitung.java
@@ -0,0 +1,21 @@
+package id.ac.unpas.QuizKPL;
+
+public class Hitung {
+	public int hitungTambah(int num1, int num2) {
+		int hasil = num1+ num2;
+		return hasil;
+	}
+	public int hitungKurang(int num1, int num2) {
+		int hasil = num1 - num2;
+		return hasil;
+	}
+
+	public int hitungBagi(int num1, int num2) {
+		int hasil = num1 / num2;
+		return hasil;
+	}
+	public int hitungKali(int num1, int num2) {
+		int hasil = num1 * num2;
+		return hasil;
+	}
+}
