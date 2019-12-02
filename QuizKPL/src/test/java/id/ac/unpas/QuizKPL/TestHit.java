e5a4ab61d6276c2aee45ca2c021c82ba8f653ae9
SB604-15 <SB604-15@DESKTOP-F0E9H4G>
2019-12-02 14:22:34 +0700

    ygfytytyt

---
+++ QuizKPL/src/test/java/id/ac/unpas/QuizKPL/TestHit.java
@@ -0,0 +1,19 @@
+package id.ac.unpas.QuizKPL;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestHit {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	public void testProcess1() {
+		Hitung hitung = new Hitung();
+		int ketemu = hitung.hitungTambah(1, 2);
+		assertEquals( ketemu, 3);
+		
+	}
+}
